4355c42618fec8b31534d202d4b8f1c8947075fc
Dmitrij Vinokour <dmitrij.vinokour@cojodi.com>
2023-07-23 17:38:28 +0200

    test(variant,optional): init

--- tests/optional_test.cpp
+++ tests/optional_test.cpp
@@ -3,7 +3,28 @@
 #include <nlohmann/json.hpp>
 #include <nlohmann/json_ext.hpp>
 
+#include "utils.hpp"
+
+using nlohmann::json;
+
+struct Optional
+{
+    // clang-format off
+    NLOHMANN_SERIALIZE(Optional,
+        ((std::optional<int>, opt))
+    );
+    // clang-format on
+};
 
 TEST(TestOptional, Okay)
 {
+    auto j = JSON({"opt" : null});
+    auto obj = Optional();
+    EXPECT_EQ(j, static_cast<json>(obj));
+    EXPECT_EQ(j.dump(), static_cast<json>(obj).dump());
+
+    j = JSON({"opt" : 42});
+    obj = j.get<Optional>();
+    EXPECT_EQ(j, static_cast<json>(obj));
+    EXPECT_EQ(j.dump(), static_cast<json>(obj).dump());
 }
--- tests/variant_test.cpp
+++ tests/variant_test.cpp
@@ -3,6 +3,43 @@
 #include <nlohmann/json.hpp>
 #include <nlohmann/json_ext.hpp>
 
+struct A
+{
+    // clang-format off
+    NLOHMANN_SERIALIZE_STRICT(A,
+        ((int, a))
+    )
+    // clang-format on
+};
+
+struct B
+{
+    // clang-format off
+    NLOHMANN_SERIALIZE_STRICT(B,
+        ((int, a))
+        ((int, b))
+    )
+    // clang-format on
+};
+
+using AB = std::variant<A, B>;
+using BA = std::variant<B, A>;
+
 TEST(TestVariant, Okay)
 {
+    auto j = JSON({"a" : 1});
+
+    auto ab = j.get<AB>();
+    EXPECT_TRUE(std::holds_alternative<A>(ab));
+
+    auto ba = j.get<BA>();
+    EXPECT_TRUE(std::holds_alternative<A>(ba));
+
+    j = JSON({"a" : 1, "b" : 2});
+
+    ab = j.get<AB>();
+    EXPECT_TRUE(std::holds_alternative<B>(ab));
+
+    ba = j.get<BA>();
+    EXPECT_TRUE(std::holds_alternative<B>(ba));
 }
